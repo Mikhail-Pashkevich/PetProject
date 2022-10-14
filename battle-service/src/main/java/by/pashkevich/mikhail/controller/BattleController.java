564b87651e76f01d691feba0d531847eaec61ef0
Михаил <pashkevich.m.a@mail.ru>
2022-10-14 21:19:49 +0300

    moved mapper logic to controller

--- battle-service/src/main/java/by/pashkevich/mikhail/controller/BattleController.java
+++ battle-service/src/main/java/by/pashkevich/mikhail/controller/BattleController.java
@@ -1,9 +1,13 @@
 package by.pashkevich.mikhail.controller;
 
+import by.pashkevich.mikhail.model.User;
 import by.pashkevich.mikhail.model.dto.BattleDto;
 import by.pashkevich.mikhail.model.dto.PlayerDto;
+import by.pashkevich.mikhail.model.entity.Battle;
 import by.pashkevich.mikhail.model.entity.enums.Value;
 import by.pashkevich.mikhail.service.BattleService;
+import by.pashkevich.mikhail.service.mapper.BattleMapper;
+import by.pashkevich.mikhail.service.mapper.PlayerMapper;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.*;
 
@@ -15,26 +19,43 @@ import java.util.List;
 public class BattleController {
     private final BattleService battleService;
 
+    private final PlayerMapper playerMapper;
+    private final BattleMapper battleMapper;
+
 
     @PostMapping(headers = "create")
     public BattleDto create(@RequestBody PlayerDto playerDto) {
-        return battleService.create(playerDto);
+        User user = playerMapper.toUser(playerDto);
+
+        Battle battle = battleService.create(user);
+
+        return battleMapper.toBattleDto(battle);
     }
 
     @PostMapping(headers = "join")
     public BattleDto join(@RequestBody PlayerDto playerDto) {
-        return battleService.join(playerDto);
+        User user = playerMapper.toUser(playerDto);
+
+        Battle battle = battleService.join(user);
+
+        return battleMapper.toBattleDto(battle);
     }
 
     @PostMapping(headers = {"move", "value"})
     public BattleDto makeMove(@RequestBody BattleDto battleDto,
                               @RequestHeader("move") Integer step,
                               @RequestHeader("value") Value value) {
-        return battleService.makeMove(battleDto, step, value);
+        Battle battle = battleMapper.toBattle(battleDto);
+
+        battle = battleService.makeMove(battle, step, value);
+
+        return battleMapper.toBattleDto(battle);
     }
 
     @GetMapping
     public List<BattleDto> openedNow() {
-        return battleService.getOpenedNow();
+        List<Battle> battleList = battleService.getOpenedNow();
+
+        return battleMapper.toBattleDtoList(battleList);
     }
 }
