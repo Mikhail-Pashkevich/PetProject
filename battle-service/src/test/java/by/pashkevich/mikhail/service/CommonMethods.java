dade702d87760267f12456865090e45bc2893fe7
Михаил <pashkevich.m.a@mail.ru>
2022-10-15 17:52:49 +0300

    added test for FieldVerifyService

---
+++ battle-service/src/test/java/by/pashkevich/mikhail/service/CommonMethods.java
@@ -0,0 +1,17 @@
+package by.pashkevich.mikhail.service;
+
+import by.pashkevich.mikhail.model.entity.enums.Value;
+
+public class CommonMethods {
+    public static Long anyId() {
+        return 0L;
+    }
+
+    public static Integer anyStep() {
+        return 1;
+    }
+
+    public static Value anyValue() {
+        return Value.VALUE_X;
+    }
+}
