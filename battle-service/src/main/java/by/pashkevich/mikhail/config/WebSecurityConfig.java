5882241b896a8411fd14dc8ceed580dc53b25f58
Михаил <pashkevich.m.a@mail.ru>
2023-07-17 00:04:18 +0300

    Revert "drop unused feign client and filter, add auth manager and liquibase script"
    This reverts commit e387af4ce78139446927da40022b1b7188c064c8.

--- battle-service/src/main/java/by/pashkevich/mikhail/config/WebSecurityConfig.java
+++ battle-service/src/main/java/by/pashkevich/mikhail/config/WebSecurityConfig.java
@@ -1,6 +1,6 @@
 package by.pashkevich.mikhail.config;
 
-import by.pashkevich.mikhail.security.JwtAuthenticationManager;
+import by.pashkevich.mikhail.security.JwtRequestFilter;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -8,6 +8,7 @@ import org.springframework.security.config.annotation.method.configuration.Enabl
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 
 @Configuration
@@ -15,19 +16,18 @@ import org.springframework.security.web.SecurityFilterChain;
 @EnableGlobalMethodSecurity(securedEnabled = true)
 @RequiredArgsConstructor
 public class WebSecurityConfig {
-
-    private final JwtAuthenticationManager jwtAuthenticationManager;
+    private final JwtRequestFilter jwtRequestFilter;
 
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         return http
                 .cors().disable()
                 .csrf().disable()
-                .authorizeRequests().anyRequest().authenticated()
+                .authorizeRequests()
+                .anyRequest().authenticated()
                 .and()
                 .formLogin().disable()
-                .oauth2ResourceServer().jwt().authenticationManager(jwtAuthenticationManager).and()
-                .and()
+                .addFilterBefore(jwtRequestFilter, UsernamePasswordAuthenticationFilter.class)
                 .build();
     }
 }
