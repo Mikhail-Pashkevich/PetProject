a30ef4fbee110be007986951a05d7fc21f621e18
Михаил <pashkevich.m.a@mail.ru>
2023-07-17 00:04:33 +0300

    Revert "add google auth"
    This reverts commit 7dc77e86245687c2b4c6a3a0aaa907b59f0703d5.

--- user-service/src/main/java/by/pashkevich/mikhail/security/JwtRequestFilter.java
+++ user-service/src/main/java/by/pashkevich/mikhail/security/JwtRequestFilter.java
@@ -1,9 +1,14 @@
 package by.pashkevich.mikhail.security;
 
-import by.pashkevich.mikhail.util.SecurityUtil;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpHeaders;
+import org.springframework.security.authentication.AbstractAuthenticationToken;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.web.authentication.WebAuthenticationDetailsSource;
 import org.springframework.stereotype.Component;
+import org.springframework.util.StringUtils;
 import org.springframework.web.filter.OncePerRequestFilter;
 
 import javax.servlet.FilterChain;
@@ -23,7 +28,7 @@ public class JwtRequestFilter extends OncePerRequestFilter {
     protected void doFilterInternal(HttpServletRequest request,
                                     HttpServletResponse response,
                                     FilterChain filterChain) throws ServletException, IOException {
-        String jwt = SecurityUtil.getAuthValue(request, SecurityUtil.RequestType.BEARER);
+        String jwt = getJwt(request);
 
         if (jwt == null) {
             log.info("REQUEST DON'T HAVE JWT");
@@ -39,13 +44,27 @@ public class JwtRequestFilter extends OncePerRequestFilter {
         }
 
         try {
-            SecurityUtil.setAuthentication(jwt, request);
-            log.info("USER IS AUTHORIZED");
+            setJwt(jwt, request);
             session.setAttribute(JWT, jwt);
             filterChain.doFilter(request, response);
         } catch (Exception e) {
             log.info("USER IS NOT AUTHORIZED");
             filterChain.doFilter(request, response);
         }
+    }
+
+    private String getJwt(HttpServletRequest request) {
+        String headerAuth = request.getHeader(HttpHeaders.AUTHORIZATION);
+        if (StringUtils.hasText(headerAuth) && headerAuth.startsWith("Bearer ")) {
+            return headerAuth.substring(7);
+        }
+        return null;
+    }
+
+    private void setJwt(String jwt, HttpServletRequest request) {
+        AbstractAuthenticationToken authentication = new UsernamePasswordAuthenticationToken(jwt, null, null);
+        authentication.setDetails(new WebAuthenticationDetailsSource().buildDetails(request));
+        SecurityContextHolder.getContext().setAuthentication(authentication);
+        log.info("USER IS AUTHORIZED");
     }
 }
