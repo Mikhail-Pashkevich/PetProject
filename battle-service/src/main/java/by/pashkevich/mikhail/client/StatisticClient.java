0581ab59ece86fa20a9012c693b8448e2c4dfb93
Михаил <pashkevich.m.a@mail.ru>
2023-09-20 12:00:36 +0300

    add new field and fix mistakes

--- battle-service/src/main/java/by/pashkevich/mikhail/client/StatisticClient.java
+++ battle-service/src/main/java/by/pashkevich/mikhail/client/StatisticClient.java
@@ -3,9 +3,10 @@ package by.pashkevich.mikhail.client;
 import by.pashkevich.mikhail.dto.StatisticDto;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 
 @FeignClient(name = "statisticClient", url = "${app.vars.statistic.url}")
 public interface StatisticClient {
     @PostMapping(value = "/statistic")
-    void save(StatisticDto statisticDto);
+    void save(@RequestBody StatisticDto statisticDto);
 }
--- battle-service/src/main/java/by/pashkevich/mikhail/dto/StatisticDto.java
+++ battle-service/src/main/java/by/pashkevich/mikhail/dto/StatisticDto.java
@@ -3,6 +3,8 @@ package by.pashkevich.mikhail.dto;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import java.time.LocalDateTime;
+
 @NoArgsConstructor
 @Data
 public class StatisticDto {
@@ -10,6 +12,7 @@ public class StatisticDto {
     private Object[] args;
     private Object result;
     private String exception;
+    private final LocalDateTime timestamp = LocalDateTime.now();
 
     public StatisticDto(String message, Object[] args, Object result) {
         this(message, args, result, null);
