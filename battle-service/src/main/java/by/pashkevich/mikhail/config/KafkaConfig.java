a4b203b0529a679d3709fc36b8642e5392f0a985
Михаил <pashkevich.m.a@mail.ru>
2023-10-05 10:28:57 +0300

    drop unnecessary config

--- battle-service/src/main/java/by/pashkevich/mikhail/config/KafkaConfig.java
+++ battle-service/src/main/java/by/pashkevich/mikhail/config/KafkaConfig.java
@@ -3,22 +3,11 @@ package by.pashkevich.mikhail.config;
 import org.apache.kafka.clients.admin.NewTopic;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.kafka.core.KafkaOperations;
-import org.springframework.kafka.listener.CommonErrorHandler;
-import org.springframework.kafka.listener.DeadLetterPublishingRecoverer;
-import org.springframework.kafka.listener.DefaultErrorHandler;
-import org.springframework.util.backoff.FixedBackOff;
 
 @Configuration
 public class KafkaConfig {
 
     public static final String STATISTIC_TOPIC_NAME = "statistics";
-
-    @Bean
-    public CommonErrorHandler errorHandler(KafkaOperations<Object, Object> template) {
-        return new DefaultErrorHandler(
-                new DeadLetterPublishingRecoverer(template), new FixedBackOff(1000L, 2));
-    }
 
     @Bean
     public NewTopic statistics() {
